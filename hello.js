7a9c528f50688fa6cb96c8b6717162c11f05829e
CodeJ2021 <rqcast5654@sbcglobal.net>
2021-02-07 17:10:33 +0000

    Updated file to print Hello World!

--- hello.js
+++ hello.js
@@ -1,7 +1,8 @@
-function hello() {
+function hello(){
 	// TODO: change this string so that your program prints "Hello world!" 
 	// when you hit the replit run button
-	return "Hello!";
+	return "Hello World!";
 }
+
+module.exports = hello
  
-module.exports = hello;
